7c57294aee5ed4e1c7dbfdb1f2ad44d4a6c1677c
jamisonlewin <jamisonlewin@gmail.com>
2021-08-05 11:57:58 -0700

    saving with weather data displaying

--- src/Weather.js
+++ src/Weather.js
@@ -9,8 +9,10 @@ class Weather extends React.Component {
       <>
         {this.props.weatherDataInfoArr.length > 0 ? this.props.weatherDataInfoArr.map((value, idx) => (
           <div key={idx}>
-            <h2>{value.date}</h2>
-            <p>{value.description}</p>
+            <h2>DATE: {value.date}</h2>
+            <p>Clouds: {value.description}</p>
+            <p>Low Temp: {value.lowTemp}</p>
+            <p>High Temp: {value.highTemp}</p>
           </div>
         ))
           : ''}
